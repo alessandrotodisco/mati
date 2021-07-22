db2f16eca02c214bf610bfec5a6d37b069c70df2
Alessandro Todisco <todisco.ale@gmail.com>
2021-07-22 23:53:50 +0200

    Final structure

---
+++ products.php
@@ -0,0 +1,145 @@
+<?php require('header.php') ?>
+
+<div class="container">
+    <h3>Materiali</h3>
+    <p>Di seguito un elenco dei materiali utilizzati per la realizzazione
+        di guarnizioni ed articoli tecnici.</p>
+    <div class="row padBox-materiali">
+        <div class="col-sm">
+            <h4>Gomma</h4>
+            <ul id="ul-gomma">
+                <li>NBR</li>
+                <li>SBR</li>
+                <li>NR</li>
+                <li>CR</li>
+                <li>EPDM</li>
+                <li>FKM</li>
+                <li>VMQ</li>
+                <li>ANTIABRASIVE</li>
+                <li>EPDM E NBR ALIMENTARE</li>
+                <li>SILICONE Trasparente e Rosso</li>
+            </ul>
+        </div>
+        <div class="col-sm">
+            <div id="carousel-gomma" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="img/banner1.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/banner2.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/banner3.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <br>
+
+        <h4>Gomma espansa e siliconi espansi</h4>
+        <ul id="ul-gomsil">
+            <li>SILICONE Espanso</li>
+            <li>EPDM Espanso</li>
+            <li>CR Espanso</li>
+            <li>NBR Espanso (anche con adesivo)</li>
+        </ul>
+
+        <br>
+
+        <div class="col-sm">
+            <div id="carousel-altro" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="img/banner1.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/banner2.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/banner3.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+
+            <h4>E molti altri prodotti...</h4>
+            <ul id="ul-altro">
+                <li>PVC compatto e semi espanso (FOREX)</li>
+                <li>Pe e PC espanso varie densità</li>
+                <li>Feltro Lana</li>
+                <li>Tessuti Gommati per membrane</li>
+                <li>Sughero Gomma</li>
+                <li>Adiprene, Poliuretano e Poliuretano espanso</li>
+                <li>Ptfe e Ptfe Espanso</li>
+                <li>Nastri in Ptfe</li>
+                <li>Profili adesivi e Corde in Ptfe Espanso</li>
+                <li>Giunture in Esente Amianto</li>
+                <li>Mylar, Film in Poliestere</li>
+                <li>Grafite Pura o Rinforzata</li>
+                <li>Boccole o Anelli in Grafite Pura</li>
+                <li>Carte oleo resistenti</li>
+                <li>Cartoni Dielettrici</li>
+                <li>Cartoni con fibre termoresistenti per alte temperature</li>
+                <li>Carta Ceramica Ecologica</li>
+                <li>Feltri tecnici alte temperature</li>
+                <li>Baderne e anelli preformati per premistoppa</li>
+            </ul>
+        </div>
+    </div>
+
+    <br /><br />
+
+    <h3>Lavorazioni</h3>
+    <p>Di seguito un elenco dei macchinari impiegati per la realizzazione di guarnizioni ed articoli tecnici.</p>
+    <br><br>
+    <div class="row padBox-lavorazioni">
+        <div class="col-sm">
+            <div>
+                <h4>Taglio laser</h4>
+
+            </div>
+        </div>
+        <div class="col-sm">
+            <div id="carousel-gomma" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="https://www.facs.it/wp-content/uploads/2019/01/Taglio-laser.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://globalmetalsrl.it/wp-content/uploads/2016/03/azienda_slide3.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require('footer.php') ?>
